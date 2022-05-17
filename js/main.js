4901f49641f48283e662c5a7f44738ee1b0f7904
pedrocore22 <pedrocore22@itweblearning.com>
2022-05-17 09:12:16 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Arrays en JavaScript
+
+let frutas = ['pera','manzana','naranja'];
+console.log(frutas);
+
+// Para acceder a los valores de un Array
+// utilizamos su índice (que comienza en 0)
+
+// Para leer valores [posición ó expresión]
+
+console.log(frutas[0]);
+
+console.log(frutas[frutas.length -1]);
+
+// Para escribir (setear) valores 
+
+frutas[1] = 'albaricoque';
+console.log(frutas);
